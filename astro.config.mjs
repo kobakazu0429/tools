612048ae42ebf5ad1626ecb2bee8506f218a6d4a
kobakazu0429 <kobakazu10@gmail.com>
2024-06-23 13:49:23 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kobakazu0429.github.io",
-  base: import.meta.env.PROD ? "/tools" : undefined,
+  base: import.meta.env.PROD ? "/tools/" : undefined,
   integrations: [tailwind()],
 });
